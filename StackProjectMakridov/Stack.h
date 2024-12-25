c29456b868fb465ca2fd0e86d17b947f58c65aa1
AnmariX <167972256+AnmariX@users.noreply.github.com>
2024-12-25 17:56:04 +0300

    Update Stack.h

--- StackProjectMakridov/Stack.h
+++ StackProjectMakridov/Stack.h
@@ -2,47 +2,64 @@
 #ifndef STACK_H
 #define STACK_H
 
-#include <cstddef> // Для size_t
-#include <stdexcept> // Для исключений
+#include <cstddef> 
+#include <stdexcept> 
 
-// Шаблонный класс для реализации стека
+/**
+* @brief Класс для реализации стека
+*/
 template <typename T>
 class Stack {
 private:
     struct Node {
-        T data;         // Данные узла
-        Node* next;     // Указатель на следующий узел
+        /**
+        * @brief Данные узла
+        * Указатель на следующий узел
+        */
+        T data;          
+        Node* next;     
 
         Node(const T& value, Node* nextNode = nullptr)
             : data(value), next(nextNode) {}
     };
-
-    Node* topNode;     // Указатель на верхний элемент стека
-    size_t stackSize;  // Количество элементов в стеке
-
+    /**
+    * @brief Указатель на верхний элемент стека
+    * Количество элементов в стеке
+    */
+    Node* topNode;      
+    size_t stackSize;   
+    
 public:
-    // Конструктор
+   /**
+   * @brief Конструктор
+   */
     Stack();
-
-    // Деструктор
+    /**
+    * @brief Деструктор
+    */
     ~Stack();
-
-    // Добавление элемента в стек
+    /**
+    * @brief Добавление элемента в стек
+    */
     void push(const T& value);
-
-    // Удаление элемента из стека
+    /**
+    * @brief Удаление элемента из стека
+    */
     void pop();
-
-    // Получение верхнего элемента стека
+    /**
+    * @brief Получение верхнего элемента стека
+    */
     T top() const;
-
-    // Проверка, пуст ли стек
+    /**
+    * @brief Проверка, пуст ли стек
+    */
     bool isEmpty() const;
-
-    // Получение размера стека
+    /**
+    * @brief Получение размера стека
+    */
     size_t size() const;
 };
 
-#include "Stack.cpp" // Для шаблонов реализация должна быть доступна в заголовочном файле
+#include "Stack.cpp" 
 
 #endif 
